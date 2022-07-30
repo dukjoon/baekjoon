731a2403a0eb11a262c367a739ab230216d9c5e6
[SW2/서울/2반] 정덕준 <jungdukjoon@gmail.com>
2022-07-30 20:14:12 +0900

    20220730

---
+++ 10950.py
@@ -0,0 +1,9 @@
+t = int(input())
+result = 0
+for i in range(t):
+    a, b = map(int,input().split())
+    print(a+b)
+
+#result = []
+# result.append(sum) 해서 다시 해보기
+
--- 2480.py
+++ 2480.py
@@ -5,16 +5,16 @@ result = 0
 if a==b==c:
     result= 10000 + (a*1000)
 
-if a==b:
+elif a==b:
     result= 1000 + (a*100)
 
-if b==c:
+elif b==c:
     result = 1000 + (b*100)
 
-if c==a:
+elif c==a:
     result = 1000 + (c*100)
 
-else:
+elif a!=b!=c:
     result = max(a, b, c) *100
 
 print(result)
